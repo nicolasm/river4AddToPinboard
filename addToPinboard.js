b30cb35196cb99cbae4500dc21964dcb854cc847
Nicolas <me@nclsmr.com>
2015-11-07 11:38:23 +0100

    Create addToPinboard.js script

---
+++ addToPinboard.js
@@ -0,0 +1,40 @@
+var fs = require ("fs");
+var Pinboard = require('node-pinboard');
+
+var now = new Date ();
+var fnameConfig = "./config.json";
+
+function loadConfig (callback) {
+    fs.readFile(fnameConfig, function (err, data) {
+        if (!err) {
+            var config = JSON.parse(data.toString ());
+            apiToken = config.pinboardUser + ':' + config.pinboardToken;
+            
+            if (callback !== undefined) {
+	            callback ();
+	        }
+        }
+    });
+}
+
+loadConfig(function() {
+    var pinboard = new Pinboard(apiToken);
+
+    var link = itemFromRiver.permaLink;
+    if (link === undefined) {
+        link = itemFromRiver.link
+    }
+
+    var options = {
+        url: link,
+        description: itemFromRiver.title,
+        extended: itemFromRiver.description,
+        tags: 'from:river4',
+	replace: 'no',
+        toread: 'no'
+    };
+
+    pinboard.add(options, function(res) {
+        console.log(res);
+    });
+});
